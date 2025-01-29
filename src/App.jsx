9182f13870871413df3fc924f060e3617576b271
Jacky Perez <jackyyp2402@gmail.com>
2025-01-28 16:34:14 -0800

    style(sidenav): cleaned up the code and added folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import './SideNav/SideNav.jsx'
-import SideNav from './SideNav/SideNav.jsx'
-import Layout from './Layout/Layout.jsx'
+import './components/SideNav/SideNav.jsx'
+import SideNav from './components/SideNav/SideNav.jsx'
+import Layout from './components/Layout/Layout.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Layout>
