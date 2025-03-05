69df8f85815641db21a7fc141b9c306174dbe9c4
Jacky Perez <jackyyp2402@gmail.com>
2025-03-04 16:45:43 -0800

    feat(auth): connected oauth and firebase

--- src/api/firebase.config.js
+++ src/api/firebase.config.js
@@ -1,29 +1,71 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
+import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getAuth } from "firebase/auth";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithPopup,
+  signInWithCredential,
+  GoogleAuthProvider,
+  signOut,
+} from 'firebase/auth';
+
+import React, { useEffect, useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { gapi } from 'gapi-script'; //Import the Google API client
+import { initClient } from './gapi';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCD2uPxkn8RmRKx2pUMDJJQ1TrBdlHDVmU",
-  authDomain: "officeflow-449417.firebaseapp.com",
-  projectId: "officeflow-449417",
-  storageBucket: "officeflow-449417.firebasestorage.app",
-  messagingSenderId: "6291519967",
-  appId: "1:6291519967:web:013f7d24ee1c3b8ce22c6a"
+  apiKey: 'AIzaSyCD2uPxkn8RmRKx2pUMDJJQ1TrBdlHDVmU',
+  authDomain: 'officeflow-449417.firebaseapp.com',
+  projectId: 'officeflow-449417',
+  storageBucket: 'officeflow-449417.firebasestorage.app',
+  messagingSenderId: '6291519967',
+  appId: '1:6291519967:web:013f7d24ee1c3b8ce22c6a',
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+//export const auth = getAuth;
+export const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-async function getUsers(db) {
+export const auth = getAuth(app);
+
+/*const [isAuthorized, setIsAuthorized] = useState(false);
+const [events, setEvents] = useState([]);
+
+// Initialize the Google API client
+useEffect(() => {
+  gapi.load('client:auth2', initClient);
+}, []);
+
+// Sign in and authorize the app
+export const handleSignIn = async () => {
+  const googleAuth = gapi.auth2.getAuthInstance();
+  const googleUser = await googleAuth.signIn();
+
+  const token = googleUser.getAuthResponse().id_token;
+
+  const credential = GoogleAuthProvider.credential(token);
+
+  await auth.signInWithCredential(credential);
+};
+
+// Sign out of the app
+export const handleSignOut = async () => {
+  await signOut(auth);
+};*/
+
+//DATABASE EXAMPLE
+/*async function getUsers(db) {
   const users = collection(db, 'users');
   const userSnapshot = await getDocs(users);
-  const userList = userSnapshot.docs.map(doc => doc.data());
+  const userList = userSnapshot.docs.map((doc) => doc.data());
   return userList;
 }
-const userList = await getUsers(db);
+const userList = await getUsers(db);*/
 //console.log(userList);
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,54 +2,10 @@ import React, { useEffect, useState } from 'react';
 import styles from './Login.module.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle } from '@fortawesome/free-brands-svg-icons';
-import { gapi } from 'gapi-script'; //Import the Google API client
-import { initClient } from '../api/gapi';
+import useAuth from '../hooks/useAuth';
 
 function Login() {
-  const [isAuthorized, setIsAuthorized] = useState(false);
-  const [events, setEvents] = useState([]);
-
-  // Initialize the Google API client
-  useEffect(() => {
-    gapi.load('client:auth2', initClient);
-  }, []);
-
-  // Sign in and authorize the app
-  const handleSignIn = () => {
-    gapi.auth2
-      .getAuthInstance()
-      .signIn()
-      .then(() => {
-        setIsAuthorized(true);
-        loadEvents();
-      });
-  };
-
-  // Sign out of the app
-  const handleSignOut = () => {
-    gapi.auth2
-      .getAuthInstance()
-      .signOut()
-      .then(() => {
-        setIsAuthorized(false);
-        setEvents([]);
-      });
-  };
-
-  // Load calendar events
-  const loadEvents = () => {
-    gapi.client.calendar.events
-      .list({
-        calendarId: 'primary',
-        timeMin: new Date().toISOString(),
-        maxResults: 10,
-        singleEvents: true,
-        orderBy: 'startTime',
-      })
-      .then((response) => {
-        setEvents(response.result.items);
-      });
-  };
+  const { handleSignIn, handleSignOut, isAuthorized, events } = useAuth();
 
   return (
     <div className={styles.animatedBackground}>
