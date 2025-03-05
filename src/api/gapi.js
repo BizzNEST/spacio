546a25d0c15dea6a95f839dabe7b44063532af97
Jacky Perez <jackyyp2402@gmail.com>
2025-03-05 10:50:39 -0800

    feat(gAPI): created and add event test

--- src/api/gapi.js
+++ src/api/gapi.js
@@ -2,7 +2,7 @@ import { gapi } from 'gapi-script'; //Import the Google API client
 
 const apiKey = import.meta.env.VITE_API_KEY;
 const clientId = import.meta.env.VITE_CLIENT_ID;
-const scope = import.meta.env.VITE_SCOPE;
+const scope = import.meta.env.VITE_SCOPES;
 
 export const initClient = () => {
   gapi.client.init({
