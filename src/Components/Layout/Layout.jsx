1e7a3f909f5ccd3a6a6b0c59d30659847c2a8df2
Jose Patino <120534408+jipatino98@users.noreply.github.com>
2025-05-28 09:59:51 -0700

    Fix: Renaming case sentivity

---
+++ src/Components/Layout/Layout.jsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import styles from './Layout.module.css';
+import SideNav from '../SideNav/SideNav';
+import Dashboard from '../Dashboard/Dashboard';
+import CalendarDashboard from '../CalendarDashboard/CalendarDashboard';
+import Header from '../Header/Header';
+import useGetCalendars from '../../api/calendars/useGetCalendars';
+import { useGetAvailability } from '../../api/availability/useGetAvailability';
+import { useFetchAllEvents } from '../../api/events/useGetEvents';
+import Loader from '../Loader/Loader';
+import useGetUserInfo from '../../api/users/useGetUserInfo';
+import { useAuth } from '../../contexts/authContext';
+
+const centerName = localStorage.getItem('center');
+
+function Layout() {
+  const { setUserInfo } = useAuth();
+  const [center, setCenter] = React.useState(`${centerName ?? 'Salinas'}`);
+  const [isCollapsed, setIsCollapsed] = React.useState(false);
+
+  //NOTE: This fetches all calendars that users are subscribed to
+  const { data: allCalendars, isLoading: isLoadingCalendars } =
+    useGetCalendars();
+  //NOTE: This fetches 25 events from all subscribed calendars
+  const { data: events, isLoading: isLoadingEvents } = useFetchAllEvents(
+    allCalendars,
+    {
+      enabled: !!allCalendars && allCalendars.length > 0,
+    }
+  );
+  //NOTE: This fetches logged in user info
+  const { data: userInfo, isLoading: isLoadingUserInfo } = useGetUserInfo();
+
+  const centerCalendars = !isLoadingCalendars
+    ? allCalendars.filter((calendar) => calendar.location === center)
+    : [];
+
+  //NOTE: This fetches availability for all calendars at center
+  const { data: calendarAvailabilities, isLoading: isLoadingAvailabilities } =
+    useGetAvailability(centerCalendars);
+
+  React.useEffect(() => {
+    if (userInfo) {
+      setUserInfo(userInfo);
+    }
+  }, [userInfo, setUserInfo]);
+
+  if (
+    isLoadingCalendars ||
+    isLoadingEvents ||
+    isLoadingUserInfo
+    // ||
+    // isLoadingAvailabilities
+  ) {
+    return <Loader label={'Preparing your dashboard...'} />;
+  }
+
+  return (
+    <div className={`${isCollapsed ? styles.layoutCollapsed : styles.layout}`}>
+      <SideNav
+        className={styles.sideNav}
+        availabilities={calendarAvailabilities}
+        center={center}
+        setCenter={setCenter}
+        isLoadingAvailabilities={isLoadingAvailabilities}
+        isCollapsed={isCollapsed}
+      />
+      <Dashboard className={styles.dashboard}>
+        <Header
+          isCollapsed={isCollapsed}
+          setIsCollapsed={setIsCollapsed}
+          centerName={center}
+        />
+        <CalendarDashboard events={events} calendars={centerCalendars} />
+      </Dashboard>
+    </div>
+  );
+}
+
+export default Layout;
---
+++ src/Components/SideNav/SideNav.jsx
@@ -0,0 +1,127 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Modal from '../Modal/Modal';
+import { faCalendarAlt, faCalendar } from '@fortawesome/free-solid-svg-icons';
+import Button from '../Button/Button';
+import styles from './SideNav.module.css';
+import CreateEventForm from '../Forms/CreateEventForm';
+import logo from '../../assets/placeholderLogo.svg';
+import AvailabilityCards from '../AvailabilityCards/AvailabilityCards';
+import NavLink from '../NavLink/NavLink';
+import 'react-datepicker/dist/react-datepicker.css';
+import SelectMenu from '../SelectMenu/SelectMenu';
+import { ClipLoader } from 'react-spinners';
+
+function SideNav({
+  availabilities,
+  center,
+  setCenter,
+  isLoadingAvailabilities,
+  isCollapsed,
+  className,
+}) {
+  const [isCreateEventModalOpen, setIsCreateEventModalOpen] =
+    React.useState(false);
+
+  const availableCalendars = availabilities.filter(
+    (calendar) => calendar.isAvailable == true
+  );
+  const busyCalendars = availabilities.filter(
+    (calendar) => calendar.isAvailable == false
+  );
+
+  return (
+    <nav className={`${className} ${isCollapsed ? styles.collapsed : ''}`}>
+      <div className={styles.topContainer}>
+        <div className={styles.logoTitle}>
+          <img
+            src={logo}
+            alt="Placeholder Logo"
+            className={
+              isCollapsed ? styles.logoCollapsed : styles.placeholderLogo
+            }
+          />
+          {!isCollapsed && <p>spacio</p>}
+        </div>
+
+        {!isCollapsed && (
+          <NavLink
+            links={[
+              {
+                path: '/home',
+                label: 'Meeting Rooms',
+                className: styles.meetingRooms,
+                icon: <FontAwesomeIcon icon={faCalendar} />,
+              },
+              // {To Do: Uncomment once Floor Plan is implemented}
+              // {
+              //   path: '/floor-map',
+              //   label: 'Floor Map',
+              //   className: styles.floorMap,
+              //   icon: <FontAwesomeIcon icon={faBuilding} />,
+              // },
+            ]}
+          />
+        )}
+
+        {!isCollapsed && (
+          <>
+            <SelectMenu center={center} setCenter={setCenter}></SelectMenu>
+            {isLoadingAvailabilities ? (
+              <div className={styles.loadingContainer}>
+                <ClipLoader />
+              </div>
+            ) : (
+              <div className={styles.availabilityContainer}>
+                {availableCalendars.length > 0 && (
+                  <AvailabilityCards
+                    header="Available"
+                    calendarList={availableCalendars}
+                  />
+                )}
+
+                {busyCalendars.length > 0 && (
+                  <AvailabilityCards
+                    header="Busy"
+                    calendarList={busyCalendars}
+                  />
+                )}
+              </div>
+            )}
+          </>
+        )}
+      </div>
+
+      <Modal
+        open={isCreateEventModalOpen}
+        onOpenChange={setIsCreateEventModalOpen}
+      >
+        <Modal.Trigger asChild>
+          <Button
+            variant={isCollapsed ? 'ghost' : 'gradient'}
+            className={isCollapsed ? styles.buttonCollapsed : styles.bookButton}
+          >
+            <FontAwesomeIcon
+              icon={faCalendarAlt}
+              className={
+                isCollapsed ? styles.calendarIconCollapsed : styles.calendarIcon
+              }
+            />
+            {!isCollapsed && <span>Book a Room</span>}
+          </Button>
+        </Modal.Trigger>
+
+        <Modal.Content
+          title={'Book a Room'}
+          subtitle={'Select your prefered time and date.'}
+        >
+          <CreateEventForm
+            calendars={availableCalendars}
+            afterSave={() => setIsCreateEventModalOpen(false)}
+          />
+        </Modal.Content>
+      </Modal>
+    </nav>
+  );
+}
+export default SideNav;
